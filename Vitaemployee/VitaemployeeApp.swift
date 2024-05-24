d6181ac6ad91ef607b9a0f311a275737f8312715
andiniverse <andiniverse@gmail.com>
2024-05-24 13:05:46 +0700

    Initial Commit

---
+++ Vitaemployee/VitaemployeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaemployeeApp.swift
+//  Vitaemployee
+//
+//  Created by Andini Malavika on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct VitaemployeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
